575f7c249d95bba70d511da1a92175d3cadcbda0
Charlie Mathews <charlie@charliemathews.com>
2013-07-28 22:05:41 -0400

    Added documentation.

--- quickload.php
+++ quickload.php
@@ -1,5 +1,5 @@
 <?php
 //Copyright 2013 Technical Solutions, LLC.
 //Confidential & Proprietary Information.
-require_once('cp.php'); $__CP = CP\Copilot::Instance(TRUE) ; require_once('/class/api_v1.php'); $__CP->ready() ;
+require_once('cp.php'); $__CP = CP\Copilot::Instance() ; require_once('/class/api_v1.php'); $__CP->ready() ;
 ?>
