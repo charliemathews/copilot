1800a66e59bd05a32ad71f57773690c7f9e74264
Charlie Mathews <charlie@charliemathews.com>
2013-07-30 10:17:04 -0400

    Fixed routes output with silenced copilot.

--- routes.php
+++ routes.php
@@ -2,7 +2,7 @@
 //Copyright 2013 Technical Solutions, LLC.
 //Confidential & Proprietary Information.
 
-require_once('quickload.php');
+require_once('quickload_silent.php');
 $routes = $__CP->returnRoutes() ;
 
 header('Content-Type: application/json');
