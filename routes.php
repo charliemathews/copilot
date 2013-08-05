514c040646be3ad390a59c78c9715bc783ef7142
Charlie Mathews <charlie@charliemathews.com>
2013-08-05 15:28:29 -0400

    Updated include paths.

--- routes.php
+++ routes.php
@@ -2,7 +2,7 @@
 //Copyright 2013 Technical Solutions, LLC.
 //Confidential & Proprietary Information.
 
-require_once('/inc/quickload_silent.php');
+require_once('inc/static.quickload_silent.php');
 $routes = $__CP->returnRoutes() ;
 
 header('Content-Type: application/json');
